3fbef17d2ee83a8035afdc1d045c3ddb109942a7
Isa Notebook <isamed92@gmail.com>
2018-09-26 20:23:55 -0300

    Fix server.js

--- server.js
+++ server.js
@@ -34,5 +34,5 @@ app.get('/about', (req, res) =>{
 // });
 
 app.listen(puerto, ()=>{
-  console.log('escuchando peticiones en el puerto 3000');
+  console.log(`escuchando peticiones en el puerto ${puerto}`);
 })
